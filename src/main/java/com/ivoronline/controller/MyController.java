33ec16dd399fbdbc11e1e242e577f1d0c34c7e18
ivoronline <ivoronline@gmail.com>
2025-02-23 21:50:52 +0100

    Commit

--- src/main/java/com/ivoronline/controller/MyController.java
+++ src/main/java/com/ivoronline/controller/MyController.java
@@ -32,7 +32,7 @@ public class MyController {
     //STORE AUTHOR/ADDRESS ENTITY INTO DB
     authorRepository.save(author);
 
-    //RETURN SOMETHING
+    //RETURN MESSAGE
     return "Author/Address was stored into DB";
 
   }
@@ -49,7 +49,7 @@ public class MyController {
     //GET ADDRESS
     Address address = author.address;
 
-    //RETURN SOMETHING
+    //RETURN MESSAGE
     return author.name + " lives in " + address.city;
     
   }
